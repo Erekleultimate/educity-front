bab0d4301aadfe20f55830530ef939ffaeffbfed
David <datogio@gmail.com>
2022-12-27 00:04:56 +0400

    feat: implement <Footer />

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -7,15 +7,48 @@ const Footer = () => {
   };
 
   return (
-    <footer>
-      <div className="paddings bg-green-500 grid grid-cols-1 md:grid-cols-4 gap-5">
-        <div className="col-span-1 md:col-span-3">
+    <footer className="text-gray-100">
+      <div className="paddings bg-green-500 grid grid-cols-1 gap-20 text-center md:grid-cols-3 md:text-left">
+        <div className="col-span-1 md:col-span-2 space-y-3">
           <div>
             <Logo color="white" />
           </div>
-          <div></div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+            excepturi ea, velit odit eligendi incidunt esse cum corrupti
+            blanditiis quaerat quia obcaecati dignissimos soluta culpa doloribus
+            cupiditate! Consectetur, qui nam.
+          </div>
         </div>
-        <div className="col-span-1 md:col-span-1">Right</div>
+        <div className="col-span-1 md:col-span-1 grid grid-cols-3">
+          <div className="col-span-1 space-y-1">
+            <div className="text-xl font-bold">კურსები</div>
+            <div className="space-y-1">
+              <div>კურსები</div>
+              <div>კურსები</div>
+              <div>კურსები</div>
+              <div>კურსები</div>
+            </div>
+          </div>
+          <div className="col-span-1 space-y-1">
+            <div className="text-xl font-bold">კურსები</div>
+            <div className="space-y-1">
+              <div>კურსები</div>
+              <div>კურსები</div>
+              <div>კურსები</div>
+              <div>კურსები</div>
+            </div>
+          </div>
+          <div className="col-span-1 space-y-1">
+            <div className="text-xl font-bold">კურსები</div>
+            <div className="space-y-1">
+              <div>კურსები</div>
+              <div>კურსები</div>
+              <div>კურსები</div>
+              <div>კურსები</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="paddings bg-green-600 text-center">
         Copyright © {getYear()} EduCity
