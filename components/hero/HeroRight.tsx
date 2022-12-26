b4cad37527872acd673f3927be655eac049b043b
David <datogio@gmail.com>
2022-12-26 23:31:52 +0400

    fix: hero image alt

--- components/hero/HeroRight.tsx
+++ components/hero/HeroRight.tsx
@@ -4,7 +4,7 @@ import img from '../../public/home-image.png';
 export const HeroRight = () => {
   return (
     <div className="col-span-1 relative h-60 md:h-auto">
-      <Image src={img} layout="fill" objectFit="cover" />
+      <Image src={img} layout="fill" objectFit="cover" alt="Hero image" />
     </div>
   );
 };
