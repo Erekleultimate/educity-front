efcd1ae803dd4ab7f7a716a70f4bc016680ed44d
David <datogio@gmail.com>
2022-12-26 21:23:02 +0400

    feat: hide navigation ul for small devices

--- components/navigaiton/Navigation.tsx
+++ components/navigaiton/Navigation.tsx
@@ -4,12 +4,12 @@ import logo from '../../public/logo.png';
 
 const Navigation = () => {
   return (
-    <header className="bg-gray-100 px-14 py-6 border-b-2 border-green-600">
+    <header className="bg-gray-100 border-b-2 border-green-600 px-3 py-6 md:px-14">
       <nav className="flex items-center justify-between">
         <div className="relative h-11 w-11">
           <Image src={logo} layout="fill" alt="logo" />
         </div>
-        <ul className="flex space-x-5 text-lg font-bold">
+        <ul className="hidden md:flex md:space-x-5 md:text-lg md:font-bold">
           <NavigaitonItem link="/" name="მთავარი" />
           <NavigaitonItem link="/library" name="ბიბლიოთეკა" />
           <NavigaitonItem link="/cources" name="კურსები" />
