f3d82bd4616c9d21c0ce66ca7310d5b3a8e24cda
David <datogio@gmail.com>
2022-12-27 16:03:47 +0400

    refactor: <SignIn />

--- components/auth/SignIn.tsx
+++ components/auth/SignIn.tsx
@@ -1,45 +1,74 @@
 import { ChangeEventHandler, MouseEventHandler, useState } from 'react';
 import { Button, Input } from '../../components';
 
-const SignIn = () => {
-  const [inputs, setInputs] = useState<{ email: string; password: string }>({
-    email: '',
-    password: '',
-  });
+interface SignInProps {
+  email: string;
+  password: string;
+  onInputChange: ChangeEventHandler<HTMLInputElement>;
+  onButtonClick: MouseEventHandler<HTMLButtonElement>;
+}
 
-  const onInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    setInputs((prev) => ({ ...prev, [event.target.name]: event.target.value }));
-  };
-
-  const onButtonClick: MouseEventHandler<HTMLButtonElement> = () => {
-    if (!inputs.email) return alert('მეილის ველის შევსება სავალდებულოა');
-    if (!inputs.password) return alert('პაროლის ველის შევსება სავალდებულოა');
-
-    alert(`${inputs.email} - ${inputs.password}`);
-    setInputs({ email: '', password: '' });
-  };
-
+const SignIn = (props: SignInProps) => {
   return (
     <div className="flex justify-center items-center h-[80vh]">
       <form
         onSubmit={(event) => event.preventDefault()}
         className="flex flex-col space-y-5 w-[80%] md:w-[30%]"
       >
-        <Input name="email" value={inputs.email} onChange={onInputChange} />
+        <Input
+          name="email"
+          value={props.email}
+          onChange={props.onInputChange}
+        />
         <Input
           name="password"
-          value={inputs.password}
-          onChange={onInputChange}
+          value={props.password}
+          onChange={props.onInputChange}
         />
         <Button
           color="white"
           size="md"
           value="Sign In"
-          onClick={onButtonClick}
+          onClick={props.onButtonClick}
         />
       </form>
     </div>
   );
 };
 
-export default SignIn;
+const WithLogic = (Component: (props: SignInProps) => JSX.Element) => {
+  return function ComponentWithLogic(): JSX.Element {
+    const [inputs, setInputs] = useState<{ email: string; password: string }>({
+      email: '',
+      password: '',
+    });
+
+    const onInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+      setInputs((prev) => ({
+        ...prev,
+        [event.target.name]: event.target.value,
+      }));
+    };
+
+    const onButtonClick: MouseEventHandler<HTMLButtonElement> = () => {
+      if (!inputs.email) return alert('მეილის ველის შევსება სავალდებულოა');
+      if (!inputs.password) return alert('პაროლის ველის შევსება სავალდებულოა');
+
+      alert(`${inputs.email} - ${inputs.password}`);
+      setInputs({ email: '', password: '' });
+    };
+
+    return (
+      <Component
+        email={inputs.email}
+        password={inputs.password}
+        onInputChange={onInputChange}
+        onButtonClick={onButtonClick}
+      />
+    );
+  };
+};
+
+const SignInWithLogic = WithLogic(SignIn);
+
+export default SignInWithLogic;
