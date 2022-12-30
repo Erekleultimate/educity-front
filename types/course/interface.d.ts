d30df0790475ef2c8e0622a872d0b0362f66054b
David <datogio@gmail.com>
2022-12-30 22:07:11 +0400

    feat: update types

---
+++ types/course/interface.d.ts
@@ -0,0 +1,12 @@
+declare namespace course {
+  interface Model {
+    id: string;
+    link: string;
+    img: string;
+    type: string;
+    place: string;
+    name: string;
+    price: string;
+    owner: string;
+  }
+}
