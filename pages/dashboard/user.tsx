d6f88a279c019a468c616ca79dba82ea11cde2ad
David <datogio@gmail.com>
2022-12-30 23:30:44 +0400

    feat: load user at dashboard user page

--- pages/dashboard/user.tsx
+++ pages/dashboard/user.tsx
@@ -1,7 +1,16 @@
+import { Dispatch, useEffect } from 'react';
 import { NextPage } from 'next';
 import { DashboardPageLaout } from '../../layouts';
+import { useDispatch } from 'react-redux';
+import * as userActions from '../../store/user';
 
 const UserDashboardPage: NextPage = () => {
+  const dispatch: Dispatch<any> = useDispatch();
+
+  useEffect(() => {
+    dispatch(userActions.setUser());
+  }, [dispatch]);
+
   return (
     <DashboardPageLaout
       pageTitle="User Dashboard"
