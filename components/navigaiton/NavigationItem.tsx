32d949e6d8e3ea24f365c882a513eab5063367fe
David <datogio@gmail.com>
2023-01-04 20:40:25 +0400

    style: bolder border for active state

--- components/navigaiton/NavigationItem.tsx
+++ components/navigaiton/NavigationItem.tsx
@@ -13,7 +13,7 @@ const NavigationItem = (props: NavigationItemProps) => {
         props.active
           ? 'bg-gray-200 text-green-600 md:bg-transparent md:border-green-600'
           : 'border-transparent hover:scale-110'
-      } border px-5 rounded-lg transition duration-300 p-1`}
+      } border-2 px-5 rounded-lg transition duration-300 p-1`}
     >
       <Link href={props.link}>
         <a className={`${props.active && 'md:cursor-default'}`}>{props.name}</a>
