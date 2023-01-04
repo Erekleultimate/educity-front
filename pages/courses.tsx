96bd224f2df3321c95f89612b7df37cb94e47767
David <datogio@gmail.com>
2023-01-04 14:07:57 +0400

    feat: implement authentication from courses page

--- pages/courses.tsx
+++ pages/courses.tsx
@@ -1,10 +1,22 @@
 import { NextPage } from 'next';
+import { useSelector } from 'react-redux';
 import { PageLayout } from '../layouts';
+import { Auth, Overlay } from '../components';
+import { useAuth } from '../hooks';
+import * as authActions from '../store/auth';
 
 const CoursesPage: NextPage = () => {
+  const { toggleAuthActivation } = useAuth();
+  const isAuthActive = useSelector(authActions.selectIsActive);
+
   return (
     <PageLayout pageTitle="Courses" pageDescription="Courses page description">
       <div>Courses Page</div>
+      {isAuthActive && (
+        <Overlay onCloseClick={toggleAuthActivation}>
+          <Auth />
+        </Overlay>
+      )}
     </PageLayout>
   );
 };
