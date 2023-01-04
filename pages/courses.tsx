f9b11ed285899eed0286be45c27ff23d07e15e9f
David <datogio@gmail.com>
2023-01-04 15:11:03 +0400

    feat: render course cards at courses page

--- pages/courses.tsx
+++ pages/courses.tsx
@@ -2,7 +2,7 @@ import { Dispatch, useEffect } from 'react';
 import { NextPage } from 'next';
 import { useDispatch, useSelector } from 'react-redux';
 import { PageLayout } from '../layouts';
-import { Auth, Overlay } from '../components';
+import { Auth, Overlay, Card } from '../components';
 import { useAuth } from '../hooks';
 import * as userActions from '../store/user';
 import * as courseActions from '../store/course';
@@ -11,6 +11,7 @@ import * as authActions from '../store/auth';
 const CoursesPage: NextPage = () => {
   const { toggleAuthActivation } = useAuth();
   const dispatch: Dispatch<any> = useDispatch();
+  const courses = useSelector(courseActions.selectAllCourses);
   const isAuthActive = useSelector(authActions.selectIsActive);
 
   useEffect(() => {
@@ -24,7 +25,21 @@ const CoursesPage: NextPage = () => {
       pageDescription="Courses page description"
       withSearch={true}
     >
-      <div className="min-h-screen">Courses Page</div>
+      <div className="min-h-screen">
+        <div className="grid grid-cols-1 gap-14 md:grid-cols-3">
+          {courses.map((course) => (
+            <Card
+              key={course.id}
+              link="/"
+              img={course.img}
+              type={course.type}
+              place={course.place}
+              name={course.name}
+              price={course.price}
+            />
+          ))}
+        </div>
+      </div>
       {isAuthActive && (
         <Overlay onCloseClick={toggleAuthActivation}>
           <Auth />
